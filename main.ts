0d392e79c83b22e6d52889b32991012bcf8d757a
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2019-01-06 14:23:35 +0900

    update

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@
  * Read more at https://makecode.microbit.org/blocks/custom
  */
 
-//% weight=100 color=#0fbc11 icon="\uf11b" block="Game Pad"
+//% weight=10 color=#0fbc11 icon="\uf11b" block="Gamepad"
 enum GamepadButton {
     //% block="Up"
     Up = EventBusSource.MICROBIT_ID_IO_P13,
