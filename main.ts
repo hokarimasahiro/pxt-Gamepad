8d9f37bd5aeb9bac787864f89148dcd9b5f5d5f7
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2019-01-06 09:08:39 +0900

    update

--- main.ts
+++ main.ts
@@ -5,10 +5,22 @@
 
 //% weight=100 color=#0fbc11 icon=""
 enum GamepadButton {
-    Down = DigitalPin.P8,
-    Right = DigitalPin.P12,
-    Up = DigitalPin.P13,
-    Left = DigitalPin.P15
+    //% block="Down Allow"
+    Down = DAL.MICROBIT_ID_IO_P8,
+    //% block="Right Allow"
+    Right = DAL.MICROBIT_ID_IO_P12,
+    //% block="Up Allow"
+    Up = DAL.MICROBIT_ID_IO_P13,
+    //% block="Left Allow"
+    Left = DAL.MICROBIT_ID_IO_P15
+}
+enum GamepadEvents {
+    //% block="pressed"
+    Down = DAL.MICROBIT_BUTTON_EVT_DOWN,
+    //% block="released"
+    Up = DAL.MICROBIT_BUTTON_EVT_UP,
+    //% block="click"
+    Click = DAL.MICROBIT_BUTTON_EVT_CLICK,
 }
 namespace Gamepad {
     let initflag: number = 0
@@ -21,73 +33,22 @@ namespace Gamepad {
     }
     /**
      * TODO: ボタンの状態を通知する
-     * @param Button ボタン。, eg: Down
+     * @param Button ボタン。, eg: Down Allow
      */
     //% blockId=Gamepad_Button_Sence block="Button Is Pressed|%Button"
     export function ButtonIsPressed(Button: GamepadButton): boolean {
         if (initflag == 0) init()
-
-        switch (Button) {
-            case GamepadButton.Down:
-                return (pins.digitalReadPin(DigitalPin.P8) == 1) ? false : true
-                break
-            case GamepadButton.Right:
-                return (pins.digitalReadPin(DigitalPin.P12) == 1) ? false : true
-                break
-            case GamepadButton.Up:
-                return (pins.digitalReadPin(DigitalPin.P13) == 1) ? false : true
-                break
-            case GamepadButton.Left:
-                return (pins.digitalReadPin(DigitalPin.P15) == 1) ? false : true
-                break
-        }
-        return false
+        return (pins.digitalReadPin(<number>Button) == 0) ? true : false
     }
     /**
      * TODO: ボタンが押されたとき
-     * @param Button ボタン。, eg: Down
+     * @param Button ボタン。, eg: Down Allow
+     * @param Event きっかけ。, eg: pressed
      * @param handler 処理。
      */
-    //% blockId=Gamepad_create_event block="on Button Pressed|%Button"
-    export function onButtonPressed(Button: GamepadButton, handler: Action) {
-        let GamepadID = 0x4100
-        let P8: number, P12: number, P13: number, P15: number
-        let lP8: number, lP12: number, lP13: number, lP15: number
-
-        lP8 = pins.digitalReadPin(DigitalPin.P8)
-        lP12 = pins.digitalReadPin(DigitalPin.P12)
-        lP13 = pins.digitalReadPin(DigitalPin.P13)
-        lP15 = pins.digitalReadPin(DigitalPin.P15)
-
-        control.onEvent(GamepadID, Button, handler);
-        if (initflag == 0) {
-            init()
-            control.inBackground(() => {
-                while (true) {
-                    P8 = pins.digitalReadPin(DigitalPin.P8)
-                    P12 = pins.digitalReadPin(DigitalPin.P12)
-                    P13 = pins.digitalReadPin(DigitalPin.P13)
-                    P15 = pins.digitalReadPin(DigitalPin.P15)
-
-                    if (lP8 == 1 && P8 == 0) {
-                        control.raiseEvent(GamepadID, GamepadButton.Down);
-                    }
-                    if (lP12 == 1 && P12 == 0) {
-                        control.raiseEvent(GamepadID, GamepadButton.Right);
-                    }
-                    if (lP13 == 1 && P13 == 0) {
-                        control.raiseEvent(GamepadID, GamepadButton.Up);
-                    }
-                    if (lP15 == 1 && P15 == 0) {
-                        control.raiseEvent(GamepadID, GamepadButton.Left);
-                    }
-                    lP8 = P8
-                    lP12 = P12
-                    lP13 = P13
-                    lP15 = P15
-                    basic.pause(50);
-                }
-            })
-        }
+    //% blockId=Gamepad_create_event block="on Button|%Button|%Event"
+    export function onButton(Button: GamepadButton,Event:GamepadEvents, handler: Action) {
+        if (initflag == 0) init()
+        control.onEvent(<number>Button,<number>Event, handler);
     }
 }
